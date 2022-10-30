cb5241f4f17a6965ed28a66c52d0d97e9680c954
Neil Stevens <neil@stevensmedia.us>
2022-10-30 15:34:25 -0400

    Make the terminal white-on-black

--- src/TerminalView.cpp
+++ src/TerminalView.cpp
@@ -3,6 +3,7 @@
 TerminalView::TerminalView(QWidget *parent)
 	: QTextEdit(parent)
 {
+	setStyleSheet("background-color: black; color: white; selection-color: black; selection-background-color: white;");
 }
 
 TerminalView::~TerminalView()
