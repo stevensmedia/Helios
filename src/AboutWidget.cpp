b92dd95902efadafb4cb65f62b0ed43e26b0d00d
Neil Stevens <neil@stevensmedia.us>
2022-11-02 19:31:45 -0400

    Add link to (future) website

--- src/AboutWidget.cpp
+++ src/AboutWidget.cpp
@@ -11,7 +11,11 @@ AboutWidget::AboutWidget(QWidget * parent)
 {
 	setLayout(new QVBoxLayout);
 
+	QLabel *link = new QLabel("<a href=\"https://heliosmush.org/\">heliosmush.org</a>", this);
+	link->setOpenExternalLinks(true);
+
 	layout()->addWidget(new QLabel(tr("Praise Helios!"), this));
+	layout()->addWidget(link);
 	layout()->addWidget(new QLabel(tr("Version") + QString(" " VERSION), this));
 	QPushButton *button = new QPushButton("Ok", this);
 	layout()->addWidget(button);
